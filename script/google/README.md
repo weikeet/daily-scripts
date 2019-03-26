ebc3f01c30e17e0d90c6b56db11f1d3cdd4f95b1
zhengwei.zhang <zhengwei.zhang@ihandysoft.com>
2019-03-26 20:02:32 +0800

    update fetch detail error data

--- script/google/README.md
+++ script/google/README.md
@@ -20,3 +20,24 @@
     ![](https://blog-1251678165.cos.ap-chengdu.myqcloud.com/2019-03-11-055128.jpg)
     - 输入需要爬取多少页数据
     - 输入账号/密码
+
+### Q & A
+1. 登录时概率出现以下错误，重新运行脚本即可
+```log
+Traceback (most recent call last):
+  File "fetch_play_console_data.py", line 220, in <module>
+    load_page_data(url, email, password, int(page_count))
+  File "fetch_play_console_data.py", line 145, in load_page_data
+    password_next.click()
+  File "/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/selenium/webdriver/remote/webelement.py", line 80, in click
+    self._execute(Command.CLICK_ELEMENT)
+  File "/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/selenium/webdriver/remote/webelement.py", line 633, in _execute
+    return self._parent.execute(command, params)
+  File "/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/selenium/webdriver/remote/webdriver.py", line 321, in execute
+    self.error_handler.check_response(response)
+  File "/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/selenium/webdriver/remote/errorhandler.py", line 242, in check_response
+    raise exception_class(message, screen, stacktrace)
+selenium.common.exceptions.WebDriverException: Message: unknown error: Element <div role="button" id="passwordNext" class="U26fgb O0WRkf zZhnYe e3Duub C0oVfc nDKKZc DL0QTb" jscontroller="VXdfxd" jsaction="click:cOuCgd; mousedown:UX7yZ; mouseup:lbsD7e; mouseenter:tfO1Yc; mouseleave:JywGue; focus:AHmuwe; blur:O22p3e; contextmenu:mg9Pef;touchstart:p6p2H; touchmove:FwuNnf; touchend:yfqBxc(preventMouseEvents=true|preventDefault=true); touchcancel:JMtRjd;" jsshadow="" jsname="Njthtb" aria-disabled="false" tabindex="0">...</div> is not clickable at point (742, 465). Other element would receive the click: <div class="ANuIbb IdAqtf" jsname="k4HEge" tabindex="0"></div>
+  (Session info: chrome=73.0.3683.86)
+  (Driver info: chromedriver=73.0.3683.68 (47787ec04b6e38e22703e856e101e840b65afe72),platform=Mac OS X 10.14.2 x86_64)
+```
--- script/google/fetch_play_console_data.py
+++ script/google/fetch_play_console_data.py
@@ -20,79 +20,76 @@ file_name = ''
 
 def get_detail_anr(html):
     soup = BeautifulSoup(html, 'lxml')
-    div_detail = soup.find('div', class_='CVP2USC-Dc-b')
-    if div_detail is not None:
-        div_detail_item = div_detail.findAll('div', class_='CVP2USC-Vp-d CVP2USC-xd-b')
-        div_error_title = div_detail_item[0].find('div', class_='gwt-HTML')
-        div_error_infos = div_detail_item[0].findAll('div', class_='gwt-Label CVP2USC-dq-b')
+    gwtHtmlDivs = soup.findAll('div', class_='gwt-HTML')
+    if gwtHtmlDivs is None or len(gwtHtmlDivs) < 2:
+        print('get_detail_anr gwtHtmlDivs is none or gwtHtmlDivs.length < 2')
+        return ''
 
-        detailErrorInfo = ' '.join(div_error_title.string.replace('\n', ' ').split()) + '\n'
-        for div_error_info in div_error_infos:
-            s1 = div_error_info.string.replace('\n', ' ')
-            s2 = ' '.join(s1.split())
-            detailErrorInfo = detailErrorInfo + s2 + '\n'
+    print(gwtHtmlDivs[1])
+    detailErrorInfo = ' '.join(str(gwtHtmlDivs[1].string).replace('\n', ' ').split()) + '\n'
+
+    gwtHtmlDiv1P = gwtHtmlDivs[1].parent
+    div_error_infos = gwtHtmlDiv1P.findAll('div', class_='gwt-Label')
+    if div_error_infos is None:
+        print('get_detail_anr div_error_infos is none')
         return detailErrorInfo
 
-    else:
-        div_detail = soup.find('div', class_='F3HRQGD-Kc-b')
-        div_detail_item = div_detail.findAll('div', class_='F3HRQGD-bq-d F3HRQGD-Qd-b')
-        div_error_title = div_detail_item[0].find('div', class_='gwt-HTML')
-        div_error_infos = div_detail_item[0].findAll('div', class_='gwt-Label F3HRQGD-hq-b')
-
-        detailErrorInfo = ' '.join(div_error_title.string.replace('\n', ' ').split()) + '\n'
-        for div_error_info in div_error_infos:
-            s1 = div_error_info.string.replace('\n', ' ')
-            s2 = ' '.join(s1.split())
-            detailErrorInfo = detailErrorInfo + s2 + '\n'
-        return detailErrorInfo
+    for div_error_info in div_error_infos:
+        s1 = div_error_info.string.replace('\n', ' ')
+        s2 = ' '.join(s1.split())
+        detailErrorInfo = detailErrorInfo + s2 + '\n'
+    return detailErrorInfo
 
 
 def get_detail_crash(html):
     soup = BeautifulSoup(html, 'lxml')
-    div_detail = soup.find('div', class_='CVP2USC-Vp-d')
-    if div_detail is not None:
-        div_error_title = div_detail.find('div', class_='gwt-HTML')
-        div_error_infos = div_detail.findAll('div', class_='gwt-Label CVP2USC-dq-b')
+    gwtHtmlDivs = soup.findAll('div', class_='gwt-HTML')
+    if gwtHtmlDivs is None or len(gwtHtmlDivs) < 1:
+        print('get_detail_crash gwtHtmlDivs is none or gwtHtmlDivs.length < 1')
+        return ''
 
-        if div_error_title.string is not None:
-            detailErrorInfo = ' '.join(div_error_title.string.replace('\n', ' ').split()) + '\n'
+    detailErrorInfo = ''
+    for gwtHtmlDiv in gwtHtmlDivs:
+        if gwtHtmlDiv.string is not None:
+            detailErrorInfo = detailErrorInfo + ' '.join(gwtHtmlDiv.string.replace('\n', ' ').split()) + '\n'
         else:
-            strong = div_error_title.find('strong')
-            detailErrorInfo = ' '.join(strong.string.replace('\n', ' ').split()) + '\n'
+            strong = gwtHtmlDiv.find('strong')
+            if strong is not None:
+                detailErrorInfo = detailErrorInfo + ' '.join(strong.string.replace('\n', ' ').split()) + '\n'
+
+        gwtHtmlDivP = gwtHtmlDiv.parent
+        div_error_infos = gwtHtmlDivP.findAll('div', class_='gwt-Label')
+        if div_error_infos is None:
+            continue
+
         for div_error_info in div_error_infos:
+            if div_error_info is None:
+                continue
             s1 = div_error_info.string.replace('\n', ' ')
             s2 = ' '.join(s1.split())
             detailErrorInfo = detailErrorInfo + s2 + '\n'
-        return detailErrorInfo
 
-    else:
-        div_detail = soup.findAll('div', class_='F3HRQGD-bq-d')
-        detailErrorInfo = ''
-        for detail in div_detail:
-            div_error_title = detail.find('div', class_='gwt-HTML')
-            div_error_infos = detail.findAll('div', class_='gwt-Label F3HRQGD-hq-b')
-
-            if div_error_title.string is not None:
-                detailErrorInfo = ' '.join(div_error_title.string.replace('\n', ' ').split()) + '\n'
-            else:
-                strong = div_error_title.find('strong')
-                detailErrorInfo = ' '.join(strong.string.replace('\n', ' ').split()) + '\n'
-            for div_error_info in div_error_infos:
-                s1 = div_error_info.string.replace('\n', ' ')
-                s2 = ' '.join(s1.split())
-                detailErrorInfo = detailErrorInfo + s2 + '\n'
-        return detailErrorInfo
+    return detailErrorInfo
 
 
 def get_table_data(session_table):
     trs = session_table.findAll('tr')
+    if trs is None:
+        print('get_table_data: table.tr tag is none')
+        return
+
     for tr in trs:
         # link
         a = tr.find('a')
         if a is None:
+            print('get_table_data: a tag is none')
             continue
 
         pv = tr.findAll('p')
+        if pv is None:
+            print('get_table_data: p tag is none')
+            continue
+
         errorDescription = ''
         errorLocation = ''
         reportsTotal = 0
@@ -128,6 +125,8 @@ def get_table_data(session_table):
 def pre_handle_data(html):
     soup = BeautifulSoup(html, 'lxml')
     session_tables = soup.findAll('section')
+    if session_tables is None:
+        print('pre_handle_data section tag is none')
     for session_table in session_tables:
         if session_table.get('role') == 'article':
             get_table_data(session_table)
@@ -160,18 +159,21 @@ def load_page_data(target_url, email, pw, page_count):
 
     print('开始时间:' + str(time.time()))
 
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div/section/section/div/div/div/span[2]/div/button[2]')))
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div/section/section/div/div/div/span[2]/div/button[2]')))
     html = driver.page_source
     pre_handle_data(html)
     print('第1页数据加载完成')
 
     curr_page = 1
     while curr_page < page_count:
-        next_page_button = driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div/section/section/div/div/div/span[2]/div/button[2]')
+        next_page_button = driver.find_element_by_xpath(
+            '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div/section/section/div/div/div/span[2]/div/button[2]')
         next_page_button.click()
 
         print('start wait next page: ' + str(time.time()))
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div/section/section/div/table/tbody[1]/tr[1]/td[5]/div/button')))
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                    '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div/section/section/div/table/tbody[1]/tr[1]/td[5]/div/button')))
         print('end wait next page: ' + str(time.time()))
 
         html = driver.page_source
@@ -187,10 +189,12 @@ def load_page_data(target_url, email, pw, page_count):
         driver.get(curr_page_url)
 
         if data_type == 0:
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div[2]/section/div[5]/div/section/div[3]/div/button')))
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,
+                                                                        '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div[2]/section/div[5]/div/section/div[3]/div/button')))
         else:
             # next page
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div[2]/section/div[5]/div/section/div[1]/div[2]/span[2]/div/button[2]')))
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,
+                                                                        '/html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/div/div/div[2]/div[2]/section/div[5]/div/section/div[1]/div[2]/span[2]/div/button[2]')))
 
         detail_html = driver.page_source
         if data_type == 0:
@@ -203,24 +207,25 @@ def load_page_data(target_url, email, pw, page_count):
     driver.close()
 
     write_data2file()
-    print('结束时间: ' + str(time.time()))
+    print('结束时间: ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
 
 if __name__ == '__main__':
     url = input('Please input anr/crash url: ')
-    page_count = input('Please input page count: ')
-    file_name = input('Please input file name: ')
-    email = input('Gmail: ')
+    email = input('Email: ')
     password = getpass.getpass('Password: ')
+    page_count = input('Please input page count: ')
 
-    # anrl
+    # anr example
     # url = 'https://play.google.com/apps/publish/?account=8505122062204140606#AndroidMetricsErrorsPlace:p=com.mobile.security.antivirus.applock.wifi&appid=4972898036482753524&appVersion=PRODUCTION&errorType=ANR'
-    # crash
+    # crash example
     # url = 'https://play.google.com/apps/publish/?account=8505122062204140606#AndroidMetricsErrorsPlace:p=com.mobile.security.antivirus.applock.wifi&appid=4972898036482753524&appVersion=PRODUCTION'
 
     if 'errorType=ANR' in url:
         data_type = 0
+        file_name = 'data_anr_' + page_count + 'page_' + time.strftime("%Y%m%d_%H.%M.%S", time.localtime())
     else:
         data_type = 1
+        file_name = 'data_crash_' + page_count + 'page_' + time.strftime("%Y%m%d_%H.%M.%S", time.localtime())
 
     load_page_data(url, email, password, int(page_count))
