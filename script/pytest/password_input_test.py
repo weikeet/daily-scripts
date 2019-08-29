2bd5b48bce03585d156f1babd1e9c77b2281b3f5
zhengwei.zhang <zhengwei.zhang@ihandysoft.com>
2019-08-29 16:46:30 +0800

    py test code

--- script/pytest/password_input_test.py
+++ script/pytest/password_input_test.py
@@ -1,7 +1,6 @@
 # -*- coding=utf-8 -*-
 import getpass
-import sys, tty, termios
 
 if __name__ == "__main__":
     pwd = getpass.getpass('password: ')
-    print pwd
+    print(pwd)
---
+++ script/pytest/py_path_test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+print('abspath:', os.path.abspath(__file__))
+print('realpath:', os.path.realpath(__file__))
+print('getcwd:', os.getcwd())
