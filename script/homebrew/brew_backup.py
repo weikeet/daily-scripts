d3c82e546cd2f0cbeba7e83ad66f65e508bd356e
zhengwei.zhang <zhengwei.zhang@ihandysoft.com>
2019-09-17 21:30:44 +0800

    add brew tag config

--- script/homebrew/brew_backup.py
+++ script/homebrew/brew_backup.py
@@ -20,7 +20,7 @@ def compare_brew_tap_list(brew_tap_list):
 
     diff_remote_with_local = []
     for remote_item in brew_tap_list:
-        
+
         if remote_item not in brew_tap_list_local:
             diff_remote_with_local.append(remote_item)
     print('未引入:', diff_remote_with_local)
@@ -34,13 +34,13 @@ def compare_brew_list(brew_list):
     brew_list_local = brew_list_text.split('\n')
     brew_list_local.remove('')
 
-    brew_list_remote = []
+    brew_list_remote_name = []
     brew_list_remote_unknown = []
     for brew_item in brew_list:
         if brew_item['tag'] == 'unknown':
             brew_list_remote_unknown.append(brew_item['name'])
         else:
-            brew_list_remote.append(brew_item['name'])
+            brew_list_remote_name.append(brew_item['name'])
     print('Unknown:', brew_list_remote_unknown)
     print('\n')
 
@@ -49,7 +49,7 @@ def compare_brew_list(brew_list):
     for local_item in brew_list_local:
         if local_item in brew_list_remote_unknown:
             pass
-        elif local_item not in brew_list_remote:
+        elif local_item not in brew_list_remote_name:
             local_item_dict = {}
             local_item_dict['name'] = local_item
             local_item_dict['desc'] = ""
@@ -60,11 +60,16 @@ def compare_brew_list(brew_list):
     print('新安装:', diff_local_with_remote)
     print('\n')
 
+    print('未安装:')
     diff_remote_with_local = []
-    for remote_item in brew_list_remote:
+    for remote_item in brew_list_remote_name:
         if remote_item not in brew_list_local:
             diff_remote_with_local.append(remote_item)
-    print('未安装:', diff_remote_with_local)
+            for brew_item in brew_list:
+                if brew_item['name'] == remote_item:
+                    print(remote_item, 'tag:', brew_item['tag'], 'desc:', brew_item['desc'])
+                    print('')
+    # print('未安装:', diff_remote_with_local)
 
     return item_list_new_install
 
@@ -75,14 +80,14 @@ def compare_brew_cask_list(brew_cask_list):
     brew_cask_list_local = brew_cask_list_text.split('\n')
     brew_cask_list_local.remove('')
 
-    brew_cask_list_remote = []
+    brew_cask_list_remote_name = []
     for brew_cask_item in brew_cask_list:
-        brew_cask_list_remote.append(brew_cask_item['name'])
+        brew_cask_list_remote_name.append(brew_cask_item['name'])
 
     diff_local_with_remote = []
     cask_item_list_new_install = []
     for local_item in brew_cask_list_local:
-        if local_item not in brew_cask_list_remote:
+        if local_item not in brew_cask_list_remote_name:
             local_item_dict = {}
             local_item_dict['name'] = local_item
             local_item_dict['desc'] = ""
@@ -95,11 +100,16 @@ def compare_brew_cask_list(brew_cask_list):
     print('新安装Cask:', diff_local_with_remote)
     print('\n')
 
+    print('未安装Cask:')
     diff_remote_with_local = []
-    for remote_item in brew_cask_list_remote:
+    for remote_item in brew_cask_list_remote_name:
         if remote_item not in brew_cask_list_local:
             diff_remote_with_local.append(remote_item)
-    print('未安装Cask:', diff_remote_with_local)
+            for brew_cask_item in brew_cask_list:
+                if brew_cask_item['name'] == remote_item:
+                    print(remote_item, 'tag:', brew_cask_item['tag'], 'desc:', brew_cask_item['desc'])
+                    print('')
+    # print('未安装Cask:', diff_remote_with_local)
 
     return cask_item_list_new_install
 
@@ -108,43 +118,47 @@ def sort_by_name(elem):
     return elem['name']
 
 
-with open('brew_backup_2019081823.json') as backup_file:
-    brew_backup_json = json.load(backup_file)
+if __name__ == '__main__':
+    is_select_update = input('is select update backup.json? yes: Y, no: N =-= ')
 
-    brew_tap_list = brew_backup_json['brew_tap']
-    brew_list = brew_backup_json['brew_list']
-    brew_cask_list = brew_backup_json['brew_cask_list']
+    with open('brew_backup_2019091720.json') as backup_file:
+        brew_backup_json = json.load(backup_file)
 
-    print('--------------------------------')
-    tap_item_list_new_install = compare_brew_tap_list(brew_tap_list)
-    print('--------------------------------\n')
-    
-    print('********************************')
-    item_list_new_install = compare_brew_list(brew_list)
-    print('********************************\n')
-    
-    print('################################')
-    cask_item_list_new_install = compare_brew_cask_list(brew_cask_list)
-    print('################################')
+        brew_tap_list = brew_backup_json['brew_tap']
+        brew_list = brew_backup_json['brew_list']
+        brew_cask_list = brew_backup_json['brew_cask_list']
 
-    if len(tap_item_list_new_install) > 0 or len(item_list_new_install) > 0 or len(cask_item_list_new_install) > 0:
-        for tap_item_new_install in tap_item_list_new_install:
-            brew_tap_list.append(tap_item_new_install)
-        for item_new_install in item_list_new_install:
-            brew_list.append(item_new_install)
-        for cask_item_new_install in cask_item_list_new_install:
-            brew_cask_list.append(cask_item_new_install)
+        print('--------------------------------')
+        tap_item_list_new_install = compare_brew_tap_list(brew_tap_list)
+        print('--------------------------------\n')
 
-        brew_list.sort(key=sort_by_name)
-        brew_cask_list.sort(key=sort_by_name)
+        print('********************************')
+        item_list_new_install = compare_brew_list(brew_list)
+        print('********************************\n')
 
-        new_brew_backup_dict = {"brew_tap": brew_tap_list,
-                                "brew_list": brew_list, "brew_cask_list": brew_cask_list}
-        new_brew_backup_json = json.dumps(
-            new_brew_backup_dict, ensure_ascii=False)
-        new_brew_backup_json_file = open(
-            'brew_backup_'+time.strftime('%Y%m%d%H', time.localtime())+'.json', 'w')
-        new_brew_backup_json_file.write(new_brew_backup_json)
-    
-    else:
-        print('All is latest')
+        print('################################')
+        cask_item_list_new_install = compare_brew_cask_list(brew_cask_list)
+        print('################################')
+
+        if is_select_update == 'Y':
+            if len(tap_item_list_new_install) > 0 or len(item_list_new_install) > 0 or len(
+                    cask_item_list_new_install) > 0:
+                for tap_item_new_install in tap_item_list_new_install:
+                    brew_tap_list.append(tap_item_new_install)
+                for item_new_install in item_list_new_install:
+                    brew_list.append(item_new_install)
+                for cask_item_new_install in cask_item_list_new_install:
+                    brew_cask_list.append(cask_item_new_install)
+
+                brew_list.sort(key=sort_by_name)
+                brew_cask_list.sort(key=sort_by_name)
+
+                new_brew_backup_dict = {"brew_tap": brew_tap_list,
+                                        "brew_list": brew_list, "brew_cask_list": brew_cask_list}
+                new_brew_backup_json = json.dumps(
+                    new_brew_backup_dict, ensure_ascii=False)
+                new_brew_backup_json_file = open(
+                    'brew_backup_' + time.strftime('%Y%m%d%H', time.localtime()) + '.json', 'w')
+                new_brew_backup_json_file.write(new_brew_backup_json)
+            else:
+                print('All is latest')
