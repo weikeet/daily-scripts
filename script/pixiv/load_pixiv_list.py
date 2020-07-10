ae0b3a74cb160c2e6f84e9412310e9983f884179
Weicools <1570682285@qq.com>
2020-07-11 00:20:34 +0800

    add len condition

--- script/pixiv/load_pixiv_list.py
+++ script/pixiv/load_pixiv_list.py
@@ -18,9 +18,7 @@ api_pixiv_member_illust = 'https://api.imjad.cn/pixiv/v1/?type=member_illust&id=
 api_pixiv_favorite = 'https://api.imjad.cn/pixiv/v1/?type=favorite&id='
 api_pixiv_rank = 'https://api.imjad.cn/pixiv/v1/?type=rank&id='
 
-# pixiv_image_folder = '/Users/zhengwei.zhang/Pictures/TestPixiv/'
 pixiv_image_folder = '/Volumes/Common/PixivImages/'
-# pixiv_image_folder = '/Volumes/Common/PixivPictures/'
 new_pixiv_image_folder = '/Volumes/Common/PixivPicturesRename1/'
 
 ID_IS_NULL = 'Id is null'
@@ -85,20 +83,12 @@ def pre_handler_file_name(suffix):
             # end_name = 'P' + file_name_s[2].split('.')[0]
             end_name = 'P' + file_name_s[2].replace(suffix, '')
         image_id_str = get_image_file_id(file_name, suffix)
-
-        new_file_name = '5229572' + '_' + image_id_str + '_' + end_name + '_NULL_' + 'mafuyu' + '_NoFound' + suffix
-
-        try:
-            os.rename(pixiv_image_folder + file_name, new_pixiv_image_folder + new_file_name)
-        except FileNotFoundError:
-            pass
-
-        # if image_id_str != ID_IS_NULL:
-        #     load_illust_detail(file_name, image_id_str, end_name, suffix)
-        # else:
-        #     print('File id format error#', file_name)
-        #     failed_item = {'file_name': file_name, 'id_str': 'None', 'error_msg': 'File id format error'}
-        #     failed_image_list.append(failed_item)
+        if image_id_str != ID_IS_NULL:
+            load_illust_detail(file_name, image_id_str, end_name, suffix)
+        else:
+            print('File id format error#', file_name)
+            failed_item = {'file_name': file_name, 'id_str': 'None', 'error_msg': 'File id format error'}
+            failed_image_list.append(failed_item)
     else:
         print('Data size error#', file_name)
         failed_item = {'file_name': file_name, 'id_str': 'None', 'error_msg': 'Data size error'}
@@ -107,23 +97,24 @@ def pre_handler_file_name(suffix):
 
 if __name__ == '__main__':
     for file_name in names:
-        if file_name.startswith('まふゆ'):
-            if file_name.endswith('.jpg'):
-                pre_handler_file_name('.jpg')
-            elif file_name.endswith('.png'):
-                pre_handler_file_name('.png')
-            elif file_name.endswith('.gif'):
-                pre_handler_file_name('.gif')
-            else:
-                if file_name != '.DS_Store':
-                    print('Not is end with .jpg/png/gif#', file_name)
-                    failed_item = {'file_name': file_name, 'id_str': 'None', 'error_msg': 'Not is end with .jpg/png/gif'}
-                    failed_image_list.append(failed_item)
+        if file_name.endswith('.jpg'):
+            pre_handler_file_name('.jpg')
+        elif file_name.endswith('.png'):
+            pre_handler_file_name('.png')
+        elif file_name.endswith('.gif'):
+            pre_handler_file_name('.gif')
+        else:
+            if file_name != '.DS_Store':
+                print('Not is end with .jpg/png/gif#', file_name)
+                failed_item = {'file_name': file_name, 'id_str': 'None', 'error_msg': 'Not is end with .jpg/png/gif'}
+                failed_image_list.append(failed_item)
 
-    failed_image_json_str = json.dumps(failed_image_list, ensure_ascii=False)
-    json_file = open('failed_image_' + time.strftime('%Y%m%d%H', time.localtime()) + '.json', 'w')
-    json_file.write(failed_image_json_str)
+    if len(failed_image_list) > 0:
+        failed_image_json_str = json.dumps(failed_image_list, ensure_ascii=False)
+        json_file = open('failed_image_' + time.strftime('%Y%m%d%H', time.localtime()) + '.json', 'w')
+        json_file.write(failed_image_json_str)
 
-    failure_image_json_str = json.dumps(failure_image_list, ensure_ascii=False)
-    failure_json_file = open('failure_image_' + time.strftime('%Y%m%d%H', time.localtime()) + '.json', 'w')
-    failure_json_file.write(failure_image_json_str)
+    if len(failure_image_list) > 0:
+        failure_image_json_str = json.dumps(failure_image_list, ensure_ascii=False)
+        failure_json_file = open('failure_image_' + time.strftime('%Y%m%d%H', time.localtime()) + '.json', 'w')
+        failure_json_file.write(failure_image_json_str)
