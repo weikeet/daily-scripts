ce31b666fec1180ce40a5a0b3f58ae68e85ad660
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2020-10-10 17:07:30 +0800

    write to json

---
+++ script/libmax/analysis_lib_config_res.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+all_module_res = []
+app_module_res = {}
+
+op_name = 'app_maxcn_android'
+pp_name = 'app_maxsecuritycn_android'
+sp_name = 'app_maxcleanercn_android'
+fc_name = 'app_fastclearcn_android'
+wk_name = 'app_walkcn_android'
+
+config_key = 'config'
+no_config_key = 'no_config'
+
+with open('lib_config_res.json', 'r') as f:
+    config_res_json = json.load(f)
+
+for name, res_list in config_res_json.items():
+    if name not in app_module_res:
+        app_module_res[name] = {}
+    if config_key not in app_module_res[name]:
+        app_module_res[name][config_key] = []
+
+    for res_src in res_list:
+        module_res = res_src.split('/')[4]
+        if module_res not in all_module_res:
+            all_module_res.append(module_res)
+        if module_res not in app_module_res[name]:
+            app_module_res[name][config_key].append(module_res)
+
+for module_res in all_module_res:
+    for name, res_map in app_module_res.items():
+        if module_res not in res_map[config_key]:
+            if no_config_key not in res_map:
+                res_map[no_config_key] = []
+            res_map[no_config_key].append(module_res)
+
+print(all_module_res)
+print(app_module_res)
+
+all_module_res_js = json.dumps(all_module_res, ensure_ascii=False)
+json_file = open('lib_config_res_export_all.json', 'w')
+json_file.write(all_module_res_js)
+
+app_module_res_js = json.dumps(app_module_res, ensure_ascii=False)
+json_file = open('lib_config_res_export_app.json', 'w')
+json_file.write(app_module_res_js)
---
+++ script/libmax/lib_config_res_change.md
@@ -0,0 +1,30 @@
+## lib_config_res.json
+1. copy `build.gradle #android #sourceSets #main #res.srcDirs`
+2. replace '' to ""
+
+## max
+### 20201009
+#### remove
+- installquestionnaire
+- luckydraw
+
+
+## max_security
+### 20201009
+#### remove
+- iap
+- incentive
+- whatsappclean
+- whatsappclean-B
+
+
+## max_cleaner
+### 20201009
+#### remove
+- iap
+- iap-B
+
+## fast_clear
+- iap
+
+## walk
