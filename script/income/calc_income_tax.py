b74b1bd6bf6855cd9dfe61c4e7e2820d7f30c58c
Weiwei <1570682285@qq.com>
2022-02-02 16:46:14 +0800

    修正年终奖计算

--- script/income/calc_income_tax.py
+++ script/income/calc_income_tax.py
@@ -67,6 +67,24 @@ def get_percent(total):
         return 0.45
 
 
+def get_year_percent(total):
+    month = round(total / 12, 2)
+    if month <= 3000:
+        return 0.03, 0
+    elif month <= 12000:
+        return 0.1, 210
+    elif month <= 25000:
+        return 0.2, 1410
+    elif month <= 35000:
+        return 0.25, 2660
+    elif month <= 55000:
+        return 0.3, 4410
+    elif month <= 80000:
+        return 0.35, 7160
+    else:
+        return 0.45, 15160
+
+
 def calc_total(lists_x, month_x):
     lists_x.append(month_x)
     total_x = 0
@@ -105,8 +123,7 @@ def calc_income(base_income, base_pay, month):
     total_income_tax_payable = calc_total(lists_income_tax_payable, month_income_tax_payable)
 
     # 当月申报税额
-    percent = 0.1
-    # percent = get_percent(total_income_tax_payable)
+    percent = get_percent(total_income_tax_payable)
     month_tax = multiply(month_income_tax_payable, percent)
     # 累计申报税额
     total_tax = calc_total(lists_tax, month_tax)
@@ -124,35 +141,64 @@ def calc_income(base_income, base_pay, month):
     # 当月实际+公积金收入
     month_real_income_accumulation_fund = plus(month_real_income, month_real_accumulation_fund)
     # 累计实际+公积金收入
-    total_real_income_accumulation_fund = calc_total(lists_real_income_accumulation_fund, month_real_income_accumulation_fund)
+    total_real_income_accumulation_fund = calc_total(lists_real_income_accumulation_fund,
+                                                     month_real_income_accumulation_fund)
 
     print('')
     print('month=', month, '---------------------------')
-    print('个人', '公积金=', accumulation_fund_pv, '养老=', old_age_insurance_pv, '医疗=', media_insurance_pv, '失业=', unemployment_insurance_pv)
-    print('公司', '公积金=', accumulation_fund_ev, '养老=', old_age_insurance_ev, '医疗=', media_insurance_ev, '失业=', unemployment_insurance_ev, '工伤=', injury_insurance_ev)
+    print('个人', '公积金=', accumulation_fund_pv, '养老=', old_age_insurance_pv, '医疗=', media_insurance_pv, '失业=',
+          unemployment_insurance_pv)
+    print('公司', '公积金=', accumulation_fund_ev, '养老=', old_age_insurance_ev, '医疗=', media_insurance_ev, '失业=',
+          unemployment_insurance_ev, '工伤=', injury_insurance_ev)
     print('当月专项扣除五险一金剩余=', month_special_deduction_income, '当月应交所得税额=', month_income_tax_payable, '当月申报税额=', month_tax)
     print('累计专项扣除五险一金剩余=', total_special_deduction_income, '累计应交所得税额=', total_income_tax_payable, '累计申报税额=', total_tax)
 
-    print('当月实际收入=', month_real_income, '当月公积金收入=', month_real_accumulation_fund, '当月实际+公积金收入=', month_real_income_accumulation_fund)
-    print('累计实际收入=', total_real_income, '累计公积金收入=', total_real_accumulation_fund, '累计实际+公积金收入=', total_real_income_accumulation_fund)
+    print('当月实际收入=', month_real_income, '当月公积金收入=', month_real_accumulation_fund, '当月实际+公积金收入=',
+          month_real_income_accumulation_fund)
+    print('累计实际收入=', total_real_income, '累计公积金收入=', total_real_accumulation_fund, '累计实际+公积金收入=',
+          total_real_income_accumulation_fund)
+
+
+def calc_awards(base_income, ppr):
+    total_award = multiply(base_income, ppr)
+
+    total_award_p = get_year_percent(total_award)
+
+    total_award_tax = subtract(multiply(total_award, total_award_p[0]), total_award_p[1])
+
+    total_award_real = subtract(total_award, total_award_tax)
+
+    print(base_income, '*', ppr, '=', total_award, '-', total_award_tax, '=', total_award_real)
+    return total_award_real
+
+
+def print_total_income(base_income, month):
+    print('\ncalc_awards')
+    total_award_real = calc_awards(base_income, month)
+
+    money = calc_total(lists_real_income, 0)
+    come_real = plus(money, total_award_real)
+    print('累计实际收入:', money, ' + ', '年终奖:', total_award_real, ' = ', come_real)
 
 
 # 基本薪资 缴纳基数
 # 01-12 薪资发放时间 02-01 次年
-def calc(base_income, total_pay, base_pay_pref, mon, base_pay_post):
+def calc(base_income, base_pay_pref, mon, base_pay_post):
     print()
     print("#########################################################################################")
-    print('####', '税前月收入=', base_income, '已累计税额=', total_pay, '存缴比例1=', base_pay_pref, '存缴比例月份=', mon, '存缴比例2', base_pay_post, '####')
+    print('####', '税前月收入=', base_income, '存缴比例1=', base_pay_pref, '存缴比例月份=', mon, '存缴比例2',
+          base_pay_post, '####')
     print("#########################################################################################")
     print()
 
     clearList()
-    lists_income_tax_payable.append(total_pay)
 
     if mon == 0:
         print('存缴比例没有变化')
         for i in range(2, 12 + 2):
             calc_income(base_income, base_pay_pref, i)
+
+        print_total_income(base_income, 4)
         return
 
     print('存缴比例变前, 存缴比例=', base_pay_pref)
@@ -167,11 +213,16 @@ def calc(base_income, total_pay, base_pay_pref, mon, base_pay_post):
     for i in range(mon, 12 + 2):
         calc_income(base_income, base_pay_post, i)
 
+    # 年终奖按照财年平均
+    print_total_income(23750, 3.0)
+    print_total_income(23750, 3.5)
+    print_total_income(23750, 4.0)
 
-# 2022.01 发放 2021.12 工资：累计税额=12806.37
 
-calc(29000, 12806.37, 18625, 7, 21250)
+calc(29000, 18625, 7, 21250)
 
-# calc(29000, 12806.37, 18625, 7, 28221)
+# calc(29000, 18625, 7, 28221)
 
-# calc(28000, 12806.37, 28000, 0, 28000)
+# calc(28000, 28000, 0, 28000)
+
+# calc(34000, 28221, 0, 28221)
