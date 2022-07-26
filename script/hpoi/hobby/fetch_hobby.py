467566ab247d5645a1eef1f0266087b50bd1127e
Weiwei <1570682285@qq.com>
2022-07-27 01:27:34 +0800

    parse hobby images

--- script/hpoi/hobby/fetch_hobby.py
+++ script/hpoi/hobby/fetch_hobby.py
@@ -1,32 +1,44 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import lxml
 import requests
+import wget
 from bs4 import BeautifulSoup
 
 
-def download(file_path, picture_url):
-    r = requests.get(picture_url)
-    with open(file_path, 'wb') as f:
-        f.write(r.content)
+# def download(file_path, picture_url):
+#     r = requests.get(picture_url)
+#     with open(file_path, 'wb') as f:
+#         f.write(r.content)
 
 
-def fetch_emoji(html_path):
-    with open(html_path, 'r') as f:
+# download("157513.html", "https://www.hpoi.net/album/157513")
+
+
+def fetch_emoji(dp, hi):
+    with open(hi, 'r') as f:
         html_str = f.read()
         soup = BeautifulSoup(html_str, 'lxml')
-        # Find element which have href attr
-        # el = soup.find(href=True)
 
-        # Print href value
-        # print(el['href'])
-        # li_emoji = soup.findAll('a', class_='emoji')
-        # for li in li_emoji:
-        #     li_img = li.find('img')
-        #     src_url = li_img['src']
-        #     print(src_url)
+        li_emoji = soup.findAll('a')
+        index = 0
+        for li in li_emoji:
+            img_url = li['href']
+            img_url_arr = img_url.split('/')
+            image_file_name = img_url_arr[-1]
+            print(image_file_name)
+            if index < 10:
+                aa = "00"+str(index)
+            elif index < 100:
+                aa = "0"+str(index)
+            else:
+                aa = str(index)
+            wget.download(img_url, out=dp + "/" + aa+"_"+image_file_name)
+            index += 1
 
-
-download("157513.html", "https://www.hpoi.net/album/157513")
-
-# fetch_emoji('img.html')
+if __name__ == '__main__':
+    print(sys.argv[1], sys.argv[2])
+    download_path = sys.argv[1]
+    hobby_img_html = sys.argv[2]
+    fetch_emoji(download_path, hobby_img_html)
